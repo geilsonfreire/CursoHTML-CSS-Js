89998ffe802ed7547e787ab91b0094989b706fd8
geilsonfreire <geilsonfreireleite@gmail.com>
2023-11-13 23:16:36 -0300

    master

---
+++ Implement_Js/Func_one_cond/index.js
@@ -0,0 +1,18 @@
+//Função de uma condição
+var a = 10;
+var b = 3;
+
+console.log("if com uma unica condição:");
+
+if (a > b) {
+    console.log("A é > que B:");
+}
+if (A == B) {
+    console.log("A é igual a B:");
+}
+if (A < B) {
+    console.log("A é menor que B:");
+}
+if (b < a) {
+    console.log("b é menor que a");
+}
